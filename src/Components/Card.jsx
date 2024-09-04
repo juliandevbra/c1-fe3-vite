ce40cf4ab84e496d429e0898470ecfee923634b3
unknown <DH@DH-CARRERA-34.DH.digitalhouse.com>
2024-09-04 15:41:01 -0300

    Clase 14: useReducer

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -6,14 +6,16 @@ import { useRecipeStates } from "../Context/Context";
 
 const Card = ({ recipe }) => {
   const { id, image, title, pricePerServing } = recipe;
-  const { setCart } = useRecipeStates();
+  const { dispatch } = useRecipeStates();
   return (
     <div className={CardStyles.cardContainer}>
       <img src={image} alt="" className={CardStyles.cardImg} />
       <h3>{title}</h3>
       <h4>{pricePerServing}</h4>
       <Counter />
-      <Button onClick={() => setCart((cart) => [...cart, recipe])}>🛒</Button>
+      <Button onClick={() => dispatch({ type: "ADD_CART", payload: recipe })}>
+        🛒
+      </Button>
       <Link to={"/detail/" + id}>
         <Button>Ver detalle</Button>
       </Link>
--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -2,12 +2,12 @@ import { useRecipeStates } from "../Context/Context";
 import Card from "../Components/Card";
 
 const Cart = () => {
-  const { cart } = useRecipeStates();
+  const { state } = useRecipeStates();
   // const { cart } = useContext(RecipeStates)
   return (
     <div>
       <h2>Recetas seleccionadas</h2>
-      {cart.map((recipe) => (
+      {state.cart.map((recipe) => (
         <Card key={recipe.id} recipe={recipe} />
       ))}
     </div>
